c1bc3f1a7832448daeb095767514d7efe59b9eb0
Parikshit <parikshitd92@gmail.com>
2018-09-17 20:22:58 -0400

    n/a

--- TestingPOC/src/Client.java
+++ TestingPOC/src/Client.java
@@ -1,4 +1,29 @@
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
 
+    public static void main(String...args) throws IOException {
+
+        Socket socket = new Socket("192.168.1.56", 4444);
+
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+        out.writeUTF("Hello, goodbye!");
+        out.flush();
+
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        String read;
+        if((read=in.readUTF())!=null){
+
+            System.out.println(read);
+        }
+
+        socket.close();
+
+    }
 
 }
--- TestingPOC/src/Server.java
+++ TestingPOC/src/Server.java
@@ -1,4 +1,46 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+    public static void main(String...args){
 
+        Socket appSocket = null;
+        ServerSocket serverSocket = null;
+        System.out.println("Server Listening------");
+
+        try {
+            serverSocket = new ServerSocket(4444);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            System.out.println("Server Hug diya!");
+        }
+
+        while(true){
+
+            try {
+                appSocket = serverSocket.accept();
+
+                System.out.println("Connected !!");
+
+                ServerThread st= new ServerThread(appSocket);
+                st.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+    }
+
+    private static class ServerThread extends Thread {
+
+        public ServerThread(Socket appSocket){
+
+        }
+    }
 }
